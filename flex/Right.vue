4b1fd7d5d67ef831dcd37cb8028bbdc7004f07a6
mikepreston17 <michael.n.preston@gmail.com>
2022-03-27 14:50:44 -0500

    Publish

---
+++ flex/Right.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-row-reverse items-center gap-2 justify-right display-block">
+    <slot />
+  </div>
+</template>
---
+++ index.js
@@ -0,0 +1,8 @@
+import Row from "./flex/Row.vue";
+import Stack from "./flex/Stack.vue";
+import Right from "./flex/Right.vue";
+import Left from "./flex/Left.vue";
+import Center from "./flex/Center.vue";
+import Flex from "./flex/Flex.vue";
+
+export { Stack, Row, Left, Right, Center, Flex };
