432b056033773ea0c02437e42f89dfe879dcf5d0
mikepreston17 <michael.n.preston@gmail.com>
2022-04-10 14:44:33 -0500

    Updated center to absolute centering

--- flex/Center.vue
+++ flex/Center.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex items-center justify-center">
+  <div class="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center">
     <slot></slot>
   </div>
 </template>
